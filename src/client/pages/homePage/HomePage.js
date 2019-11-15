cfb23b93f442c5f00edfb020df4ccc5bfcf5fac5
Oskar <oskar.leonard@hotmail.com>
2019-11-15 16:42:31 +0100

    added support for component lib

--- src/client/pages/homePage/HomePage.js
+++ src/client/pages/homePage/HomePage.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import DropzoneWithPreview from '@lvlsgroup-components/dropzones/dropzoneWithPreview/DropzoneWithPreview';
 import styles from './homePage.scss';
 
 class HomePage extends React.Component {
   render() {
     return (
       <div className={styles.homeRoute}>
+        <h1 onClick={this.handleFileRemove} className={styles.title}>
+          Home
+        </h1>
         <h1 className={styles.title}>Home</h1>
-        <h1 className={styles.title}>Home</h1>
+        <DropzoneWithPreview>
+          <div>DROP FILES HERE</div>
+        </DropzoneWithPreview>
       </div>
     );
   }
