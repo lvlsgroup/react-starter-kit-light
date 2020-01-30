455dc2216012f3cbb0c16651fdd2d14e32fce23f
Oskar <oskar.leonard@hotmail.com>
2020-01-30 14:46:17 +0100

    added support for 3 levels, oh my, this needs to be recursive

--- src/server/renderUtils.js
+++ src/server/renderUtils.js
@@ -4,35 +4,6 @@ import { matchRoutes } from 'react-router-config';
 import { get500 } from '@server/views/htmlHelpers';
 import routes from '../client/pages/routes';
 
-// The components that we're declaring in pages.js are imported async, so we can't garantee that
-// they're loaded when we want to call loadData on the server and thus we need to import them
-// staticly/synchronously. This we do with an old-school require.
-const staticRoutes = routes.map((route) => {
-  const staticComponent = require(`../client/${route.componentPath}`).default;
-  if (route.routes) {
-    let staticChildren = route.routes.map((childRoute) => {
-      const staticComponent = require(`../client/${childRoute.componentPath}`)
-        .default;
-
-      return {
-        staticComponent,
-        ...childRoute,
-      };
-    });
-
-    return {
-      staticComponent,
-      ...route,
-      routes: staticChildren,
-    };
-  } else {
-    return {
-      staticComponent,
-      ...route,
-    };
-  }
-});
-
 export function preloadRouteData(req, store) {
   return Promise.all(getRoutePromises(req.url, store));
 }
@@ -69,4 +40,52 @@ export function preloadDataErrorHandler(err, res, req) {
     res.status(status).send(get500());
     console.error(`SERVER ERROR: ${err.toString()}`);
   }
+}
+
+// The components that we're declaring in pages.js are imported async, so we can't garantee that
+// they're loaded when we want to call loadData on the server and thus we need to import them
+// staticly/synchronously. This we do with an old-school require.
+// TODO: Make it recursive so it will support loadData for more than 3 levels of child routes
+const staticRoutes = routes.map((route) => {
+  const staticComponent = requireStaticComponent(route);
+
+  if (route.routes) {
+    let staticChildren = getMappedRoutes(route.routes);
+
+    return {
+      staticComponent,
+      ...route,
+      routes: staticChildren,
+    };
+  } else {
+    return {
+      staticComponent,
+      ...route,
+    };
+  }
+});
+
+function getMappedRoutes(routes) {
+  return routes.map((childRoute) => {
+    const staticComponent = requireStaticComponent(childRoute);
+
+    if (childRoute.routes) {
+      let staticChildren = getMappedRoutes(childRoute.routes);
+
+      return {
+        staticComponent,
+        ...childRoute,
+        routes: staticChildren,
+      };
+    } else {
+      return {
+        staticComponent,
+        ...childRoute,
+      };
+    }
+  });
+}
+
+function requireStaticComponent(route) {
+  return require(`../client/${route.componentPath}`).default;
 }
