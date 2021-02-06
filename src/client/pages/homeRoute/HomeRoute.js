3f279ccde20c5df8bca356ed4116e9c11c67237f
oskarleonard <oskar.leonard@hotmail.com>
2021-02-06 14:31:39 +0100

    example on how to use theme

--- src/client/pages/homeRoute/HomeRoute.js
+++ src/client/pages/homeRoute/HomeRoute.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import classNames from 'classnames';
+import { useThemeStyles } from '@client/helperComponents/withThemeStyles/withThemeStyles';
+import Button from '@client/atomicComponents/atoms/button/Button';
 import styles from './HomeRoute.scss';
 
 class HomeRoute extends React.Component {
   render() {
+    const theme = useThemeStyles();
+
     return (
       <div className={classNames(styles.HomeRoute)}>
         <h1 onClick={this.handleFileRemove} className={styles.title}>
           Home
         </h1>
+        <div className={theme.contentContainerMd}>
+          <Button
+            className={theme.btnPrimary}
+            label={'Primary Styles Button'}
+          />
+        </div>
       </div>
     );
   }
